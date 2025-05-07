c47df5b076db2c67f52606b6bba73ddc51055756
sysypp <parkseoyoung0915@gmail.com>
2025-05-07 17:09:20 +0900

    feat:우리팀화이팅

---
+++ Controller.java
@@ -0,0 +1,10 @@
+
+public class Controller {
+	private String heebum;
+	private String minik;
+	private String sangjin;
+	private String changgyu;
+	public static void member() {
+		System.out.println("우리팀 화이팅");
+	}
+}
